932baced8dfe7dd40028cc5ef43d795ffc3cc7e9
Hannes <hannes@guedelhoefer.de>
2018-07-23 00:44:59 +0200

    :sparkles: Add confidence level calculations functions

--- actions/votes.go
+++ actions/votes.go
@@ -1,6 +1,8 @@
 package actions
 
 import (
+	"math"
+
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop"
 	"github.com/gobuffalo/uuid"
@@ -43,9 +45,25 @@ func VotesCreate(c buffalo.Context) error {
 		picture.Downvotes = picture.Downvotes + 1
 	}
 
+	picture.ConfidenceLevel = float32(confidenceLevel(picture.Upvotes, picture.Downvotes))
+
 	if err := tx.Save(&picture); err != nil {
 		return c.Render(500, r.JSON(M{"error": err.Error()}))
 	}
 
 	return c.Render(204, nil)
+}
+
+func confidenceLevel(ups uint32, downs uint32) float64 {
+	if ups == 0 {
+		if downs == 0 {
+			return 0.5
+		}
+		return float64(-downs)
+	}
+
+	n := float64(ups + downs)
+	z := float64(1.64485) //1.0 = 85%, 1.6 = 95%
+	phat := float64(ups) / n
+	return (phat + z*z/(2*n) - z*math.Sqrt((phat*(1-phat)+z*z/(4*n))/n)) / (1 + z*z/n)
 }
